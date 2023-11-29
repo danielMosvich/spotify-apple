850603ab3b229fac67053f6ac647ea3b459439f0
danielmosvich <mrdaniraymay@gmail.com>
2023-11-29 17:25:49 -0500

    gg

--- app/album/[id]/page.tsx
+++ app/album/[id]/page.tsx
@@ -3,15 +3,101 @@ import getPlaylist from "@/app/api/getAlbum";
 import { useEffect, useState } from "react";
 import { AlbumProps } from "./albumProps";
 import Image from "next/image";
-import { usePlayerStore } from "@/app/store/player";
+import { Data, usePlayerStore } from "@/app/store/player";
+
+export interface EventProps {
+  album_type:             string;
+  artists:                Artist[];
+  available_markets:      string[];
+  copyrights:             Copyright[];
+  external_ids:           ExternalIDS;
+  external_urls:          ExternalUrls;
+  genres:                 any[];
+  href:                   string;
+  id:                     string;
+  images:                 Image[];
+  label:                  string;
+  name:                   string;
+  popularity:             number;
+  release_date:           Date;
+  release_date_precision: string;
+  total_tracks:           number;
+  tracks:                 Tracks;
+  type:                   string;
+  uri:                    string;
+}
+
+export interface Artist {
+  external_urls: ExternalUrls;
+  href:          string;
+  id:            string;
+  name:          string;
+  type:          ArtistType;
+  uri:           string;
+}
+
+export interface ExternalUrls {
+  spotify: string;
+}
+
+export enum ArtistType {
+  Artist = "artist",
+}
+
+export interface Copyright {
+  text: string;
+  type: string;
+}
+
+export interface ExternalIDS {
+  upc: string;
+}
+
+export interface Image {
+  height: number;
+  url:    string;
+  width:  number;
+}
+
+export interface Tracks {
+  href:     string;
+  items:    Item[];
+  limit:    number;
+  next:     null;
+  offset:   number;
+  previous: null;
+  total:    number;
+}
+
+export interface Item {
+  artists:           Artist[];
+  available_markets: string[];
+  disc_number:       number;
+  duration_ms:       number;
+  explicit:          boolean;
+  external_urls:     ExternalUrls;
+  href:              string;
+  id:                string;
+  is_local:          boolean;
+  name:              string;
+  preview_url:       string;
+  track_number:      number;
+  type:              ItemType;
+  uri:               string;
+}
+
+export enum ItemType {
+  Track = "track",
+}
 
 export default function Album({ params }: { params: { id: string } }) {
   const [album, setAlbum] = useState<AlbumProps | null>(null);
   const { changePlayerFromAlbum } = usePlayerStore();
   const { id } = params;
-  function handleClick(e) {
+  function handleClick(e:Data) {
     if (album) {
       // console.log({ images: album.images, data: e });
+      console.log(e)
       changePlayerFromAlbum({ images: album.images, data: e });
     }
   }
